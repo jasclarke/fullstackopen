4b8b6727e4df4cf68518ff45a23db7e37936ca82
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-02-21 20:31:27 -0400

    Part 2 exercise 2.2 and 2.3 completed.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -4,12 +4,21 @@ const Part = ({part}) => <p>{part.name + " " + part.exercises}</p>
 
 const Content = ({parts}) => <>{parts.map(part => <Part key={part.id} part={part} />)}</>
 
-const Course = ({course}) => (
-  <>
-    <Header text={course.name} />
-    <Content parts={course.parts} />
-  </>
-)
+const Total = ({total}) => <p><b>Total of {total} exercises</b></p>
+
+const Course = ({course}) => {
+  const total = course.parts.reduce(
+    (previousPart, currentPart) => ({ exercises: previousPart.exercises + currentPart.exercises })
+  )
+
+  return (
+    <>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total.exercises}/>
+    </>
+  )
+}
 
 const App = () => {
   const course = {
