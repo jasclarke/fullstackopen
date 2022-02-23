4f7a5892c8006248ab73fe4eeda2fc7874be5b25
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-02-23 18:27:12 -0400

    Completed exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -13,47 +13,77 @@ const Contact = ({person}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([{
-     name: 'Arto Hellas',
-     number: '040-1234567'
-  }])
+  const data = [
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]
+
+  const [persons, setPersons] = useState({
+    contacts: [...data], 
+    filtered: [...data]
+  })
 
   const [newName, setNewName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
 
   const handleNameInput = (event) => setNewName(event.target.value)
   const handlePhoneNumberInput = (event) => setPhoneNumber(event.target.value)
 
+  const handleSearchInput = (event) => {
+    const query = event.target.value
+    setSearchName(query)
+
+    const results = persons.contacts.filter( person => 
+      person.name.toLowerCase().match(query.toLowerCase())
+    )
+    
+    const updatedPersons = {...persons}
+    updatedPersons.filtered = results
+    setPersons(updatedPersons)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
-    if (!persons.some( person => person.name === newName)) {
-      setPersons(persons.concat({
+    if (!persons.contacts.some( person => person.name === newName)) {
+      const contact = {
         name: newName,
-        number: phoneNumber
-      }))
+        number: phoneNumber,
+        id: persons.contacts.length + 1
+      }
+      const updatedPersons = {...persons}
 
+      updatedPersons.contacts.push(contact)
+      updatedPersons.filtered.push(contact)
+
+      setPersons(updatedPersons)
       setNewName('')
       setPhoneNumber('')
     } else {
       alert(`${newName} already exists in the phone book.`)
     }
   }
-
+  
+  
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <Input label='Filter Contacts' handleInput={handleSearchInput} value={searchName} />
+      <h2>New Contact</h2>
       <form>
-        <Input label='name' handleInput={handleNameInput} value={newName} />
-        <Input label='number' handleInput={handlePhoneNumberInput} value={phoneNumber} />
+        <Input label='Name' handleInput={handleNameInput} value={newName} />
+        <Input label='Phone Number' handleInput={handlePhoneNumberInput} value={phoneNumber} />
         <div>
-          <button type='submit' onClick={addPerson}>add</button>
+          <button type='submit' onClick={addPerson}>Save</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Contacts</h2>
       <p>
         {
-          persons.map( (person) => (
-            <Contact key={person.name} person={person} />
+          persons.filtered.map( person => (
+            <Contact key={person.id} person={person} />
           ))
         }
       </p>
