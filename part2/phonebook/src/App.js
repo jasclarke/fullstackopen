8a9b02ef24552700a31865805c25f2fd31136905
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-02-23 12:55:35 -0400

    Completed exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 
+const Input = ({label, value, handleInput}) => (
+  <div>
+    {label}: <input onChange={handleInput} value={value} />
+  </div>
+)
+
+const Contact = ({person}) => (
+  <>
+    <span>{person.name} {person.number}</span><br/>
+  </>
+)
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{
+     name: 'Arto Hellas',
+     number: '040-1234567'
+  }])
+
   const [newName, setNewName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+
   const handleNameInput = (event) => setNewName(event.target.value)
+  const handlePhoneNumberInput = (event) => setPhoneNumber(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
     if (!persons.some( person => person.name === newName)) {
-      setPersons(persons.concat({name: newName}))
+      setPersons(persons.concat({
+        name: newName,
+        number: phoneNumber
+      }))
+
       setNewName('')
+      setPhoneNumber('')
     } else {
       alert(`${newName} already exists in the phone book.`)
     }
@@ -19,20 +43,17 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
+        <Input label='name' handleInput={handleNameInput} value={newName} />
+        <Input label='number' handleInput={handlePhoneNumberInput} value={phoneNumber} />
         <div>
-          name: <input onChange={handleNameInput} value={newName}/>
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
+          <button type='submit' onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <p>
         {
           persons.map( (person) => (
-            <>
-              <span key={person.name}>{person.name}</span><br/>
-            </>
+            <Contact key={person.name} person={person} />
           ))
         }
       </p>
