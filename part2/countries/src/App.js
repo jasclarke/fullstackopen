92a9d4a4ec28067b8c7128a93772e51dfcc54942
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-02-26 21:29:10 -0400

    Completed exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -17,11 +17,30 @@ const List = ({countries, handleDetailsButton}) => {
 }
 
 const Details = ({country}) => {
+  const [weather, setWeather] = useState({
+    temp: '',
+    icon: '',
+    wind: ''
+  })
+
   const capital = country.capital.map(
     (city, index) => index === (country.capital.length - 1) ? city : city + ', '
   )
   
   const languages = []
+  
+  useEffect( () => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+      .then(response => {
+        setWeather({
+          temp: response.data.main.temp,
+          icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+          wind: response.data.wind.speed,
+          description: `${response.data.weather[0].description} in ${country.capital[0]}`
+        })
+      })
+  }, [country])
 
   for (const language in country.languages) {
     languages.push(<li key={country.languages[language]}>{country.languages[language]}<br/></li>)
@@ -35,6 +54,11 @@ const Details = ({country}) => {
       <h3>Languages</h3>
       <ul>{languages}</ul>
       <img src={country.flags.png} alt={'An image of ' + country.name.common + ' flag'}/>
+      <h3>Weather in {country.capital[0]}</h3>
+      <div>
+        <img src={weather.icon} alt={weather.description} />
+      </div>
+      <p>Wind {weather.wind} m/s</p>
     </>
   )
 }
