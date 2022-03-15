1876359b16448927bba608d2013fa5a02f73263d
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-03-15 19:35:15 -0400

    Completed exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogList) => {
+    if (blogList.length === 0) {
+        return 0
+    } else if (blogList.length === 1) {
+        return blogList[0].likes
+    } else {
+        return blogList.reduce((totalLikes, blog) => {
+            return typeof totalLikes === 'number' 
+                ? totalLikes += blog.likes 
+                : totalLikes.likes += blog.likes
+        })
+    }
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
