1f86863e8aa90bdc1ee4bbfcd11a08581f785fde
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-03-23 19:54:22 -0400

    Completed execrcise 4.8

---
+++ part4/tests/blogs_api.test.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./api_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogs = helper.initialBlogs.map(blog => new Blog(blog).save())
+    await Promise.all(blogs)
+}, 100000)
+
+test('ensure all blog posts are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+}, 100000)
