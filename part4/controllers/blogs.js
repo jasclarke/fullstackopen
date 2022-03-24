6512b549fae75a38a36b75209fd636a7a6286b18
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-03-24 17:10:38 -0400

    Exercise 4.13 and 4.14 completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
@@ -21,15 +22,39 @@ blogsRouter.get('/:id', async (request, response) => {
 })
   
 blogsRouter.post('/', async (request, response) => {
-    const blog = request.body
-
-    if (!blog.hasOwnProperty('likes')) {
-        blog.likes = 0
+    const blog = {
+        title: request.body.title,
+        url: request.body.url,
+        author: request.body.author || '',
+        likes: request.body.likes || 0
     }
 
     const blogToSave = new Blog(blog)
     const savedBlog = await blogToSave.save()
     response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const blog = {
+        title: request.body.title,
+        url: request.body.url,
+        author: request.body.author,
+        likes: request.body.likes
+    }
+
+    const options = {
+        new: true,
+        runValidators: true,
+        context: 'query'
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, options)
+    response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -12,61 +12,86 @@ beforeEach(async () => {
     await Promise.all(blogs)
 }, 100000)
 
-test('ensure all blog posts are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-}, 100000)
-
-test('blog posts unique identifier should be named id', async () => {
-    const response = await api.get(`/api/blogs/${helper.initialBlogs[0]._id}`)
-    expect(response.body.id).toBeDefined()
-}, 100000)
-
-test('ensure blog posts can be saved', async () => {
-    const blog = {
-        title: 'Node API Testing with Jest and Supertest',
-        url: 'http://jasonclarke.dev',
-        author: 'Jason Clarke',
-        likes: 9,
-    }
-
-    const savedBlog = await api.post('/api/blogs').send(blog)
-    const blogs = await helper.blogsInDb()
+describe('retrieving blog posts', () => {
+    test('ensure all blog posts are returned', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    }, 100000)
     
-    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
-    expect(savedBlog.body.title).toBe(blog.title)
-    expect(savedBlog.body.url).toBe(blog.url)
-    expect(savedBlog.body.author).toBe(blog.author)
-    expect(savedBlog.body.likes).toBe(blog.likes)    
-}, 100000)
+    test('blog posts unique identifier should be named id', async () => {
+        const response = await api.get(`/api/blogs/${helper.initialBlogs[0]._id}`)
+        expect(response.body.id).toBeDefined()
+    }, 100000)
+})
 
-test('ensure blog posts without the likes property gets one and is defined to zero', async () => {
-    const blog = {
-        title: 'Node API Testing with Jest and Supertest',
-        url: 'http://jasonclarke.dev',
-        author: 'Jason Clarke',
-    }
+describe('adding blog posts', () => {
+    test('ensure blog posts can be saved', async () => {
+        const blog = {
+            title: 'Node API Testing with Jest and Supertest',
+            url: 'http://jasonclarke.dev',
+            author: 'Jason Clarke',
+            likes: 9,
+        }
+    
+        const savedBlog = await api.post('/api/blogs').send(blog)
+        const blogs = await helper.blogsInDb()
+        
+        expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+        expect(savedBlog.body.title).toBe(blog.title)
+        expect(savedBlog.body.url).toBe(blog.url)
+        expect(savedBlog.body.author).toBe(blog.author)
+        expect(savedBlog.body.likes).toBe(blog.likes)    
+    }, 100000)
+    
+    test('ensure blog posts without the likes property gets one and is defined to zero', async () => {
+        const blog = {
+            title: 'Node API Testing with Jest and Supertest',
+            url: 'http://jasonclarke.dev',
+            author: 'Jason Clarke',
+        }
+    
+        const savedBlog = await api.post('/api/blogs').send(blog)
+    
+        expect(savedBlog.body.likes).toBeDefined()
+        expect(savedBlog.body.likes).toBe(0)
+    }, 100000)
+    
+    test('reject blog posts without a title or url with status code 400', async () => {
+        const blogOne = {
+            author: 'Jason Clarke'
+        }
+    
+        const blogTwo = {
+            title: 'Bad Request 400'
+        }
+    
+        const blogThree = {
+            url: 'http://badrequest400.com'
+        }
+    
+        await api.post('/api/blogs').send(blogOne).expect(400)
+        await api.post('/api/blogs').send(blogTwo).expect(400)
+        await api.post('/api/blogs').send(blogThree).expect(400)
+    }, 100000)
+})
 
-    const savedBlog = await api.post('/api/blogs').send(blog)
+describe('deleting blog posts', () => {
+    test('ensure blog posts is deleted upon request', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        await api.delete(`/api/blogs/${blogsAtStart[0].id}`).expect(204)
+        
+        const blogsAfterDeletion = await helper.blogsInDb()
+        expect(blogsAfterDeletion).toHaveLength(blogsAtStart.length - 1)
+    }, 100000)
+})
 
-    expect(savedBlog.body.likes).toBeDefined()
-    expect(savedBlog.body.likes).toBe(0)
-}, 100000)
-
-test('reject blog posts without a title or url with status code 400', async () => {
-    const blogOne = {
-        author: 'Jason Clarke'
-    }
-
-    const blogTwo = {
-        title: 'Bad Request 400'
-    }
-
-    const blogThree = {
-        url: 'http://badrequest400.com'
-    }
-
-    const savedBlogOne = await api.post('/api/blogs').send(blogOne).expect(400)
-    const savedBlogTwo = await api.post('/api/blogs').send(blogTwo).expect(400)
-    const savedBlogThree = await api.post('/api/blogs').send(blogThree).expect(400)
-}, 100000)
+describe('modifying blog posts', () => {
+    test('ensure blog posts can be updated upon request', async () => {
+        const blogs = await helper.blogsInDb()
+        const blogToUpdate = blogs[0]
+        
+        blogToUpdate.likes += 1
+        const updatedBlog = await api.put(`/api/blogs/${blogs[0].id}`).send(blogToUpdate).expect(200)
+        expect(updatedBlog.body.likes).toBe(blogToUpdate.likes)
+    }, 100000)
+})
