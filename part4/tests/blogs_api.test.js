5db7bf94c275349300f35c9b9e3d8e2eb95d6168
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-03-24 10:39:42 -0400

    Completed exercise 4.10

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -20,3 +20,21 @@ test('blog posts unique identifier should be named id', async () => {
     const response = await api.get(`/api/blogs/${helper.initialBlogs[0]._id}`)
     expect(response.body.id).toBeDefined()
 })
+
+test('ensure blog posts can be saved', async () => {
+    const blog = {
+        title: 'Node API Testing with Jest and Supertest',
+        url: 'http://jasonclarke.dev',
+        author: 'Jason Clarke',
+        likes: 9,
+    }
+
+    const savedBlog = await api.post('/api/blogs').send(blog)
+    const blogs = await helper.blogsInDb()
+    
+    expect(blogs).toHaveLength(helper.initialBlogs.length + 1)
+    expect(savedBlog.body.title).toBe(blog.title)
+    expect(savedBlog.body.url).toBe(blog.url)
+    expect(savedBlog.body.author).toBe(blog.author)
+    expect(savedBlog.body.likes).toBe(blog.likes)    
+})
