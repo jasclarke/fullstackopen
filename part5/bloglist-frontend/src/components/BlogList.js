f9e58c920314fbd846f13d5a0436d14f4ef7058b
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-05-06 10:47:50 -0400

    Completed exercise 5.9

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Blog from './Blog'
 
 const BlogList = ({blogs, updateBlog}) => {
+    let blogsCopy = [...blogs]
+    blogsCopy.sort((a, b) => b.likes - a.likes)
+
     return (
         <div>
           <h2>blogs</h2>
-          {
-            blogs.map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />)
-          }
+          { blogsCopy.map(blog => <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />) }
         </div>
     )
 }
