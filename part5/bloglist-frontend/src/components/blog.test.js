6f148b814feb4886479d1c94d52855b3c895464f
Jason Clarke <61100900+jasclarke@users.noreply.github.com>
2022-05-16 21:00:34 -0400

    Completed exercise 5.14

--- part5/bloglist-frontend/src/components/blog.test.js
+++ part5/bloglist-frontend/src/components/blog.test.js
@@ -2,25 +2,39 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
-test('Blog displays title and author only', () => {
-    const blog = {
-        title: 'This is a test',
-        author: 'Jason Clarke',
-        url: 'http://test.test',
-        likes: 0,
-        user: {
-            name: 'Jason Clarke'
+describe('Blog Component', () => {
+    let container
+
+    beforeEach(() => {
+        const blog = {
+            title: 'This is a test',
+            author: 'Jason Clarke',
+            url: 'http://test.test',
+            likes: 0,
+            user: {
+                name: 'Jason Clarke'
+            }
         }
-    }
 
-    let container = render(<Blog blog={blog} />).container
+        container = render(<Blog blog={blog} />).container
+    })
 
-    const title = screen.queryByText('This is a test')
-    const author = screen.queryByText('Jason Clarke')
-    const blogDetails = container.querySelector('.blog-details')
+    test('Blog displays title and author only', () => {
+        const title = screen.queryByText('This is a test')
+        const author = screen.queryByText('Jason Clarke')
+        const blogDetails = container.querySelector('.blog-details')
 
-    expect(title).toBeDefined()
-    expect(author).toBeDefined()
-    expect(blogDetails).toHaveStyle('display: none')
+        expect(title).toBeDefined()
+        expect(author).toBeDefined()
+        expect(blogDetails).toHaveStyle('display: none')
+    })
+
+    test('Blog details are displayed upon clicking the view button', async () => {
+        const viewBtn = screen.getByText('view')
+        userEvent.click(viewBtn)
+        const blogDetails = container.querySelector('.blog-details')
+        expect(blogDetails).not.toHaveStyle('display: none')
+    })
 })
